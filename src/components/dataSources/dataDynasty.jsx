8d86a871a9dc973a1ab668277a7620926a99f12b
Brent Ogden <ogden87@gmail.com>
2026-07-20 19:53:31 -0600

    team banner updates

--- src/components/dataSources/dataDynasty.jsx
+++ src/components/dataSources/dataDynasty.jsx
@@ -5,7 +5,7 @@ export default [
         ownerName: "Brent Ogden",
         playoffPoints: 514.55,
         teamLogo: "brent2025.png",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0001&O=07",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0001&O=07",
         record: {
             record: "9-5",
             winPercentage: ".643",
@@ -22,7 +22,7 @@ export default [
         ownerName: "Tom Courtney",
         playoffPoints: 447.10,
         teamLogo: "tom2024logo.jpg",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0002&O=07",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0002&O=07",
         record: {
             record: "7-7",
             winPercentage: ".500",
@@ -37,7 +37,7 @@ export default [
         ownerName: "Nick Hazen",
         playoffPoints: 542.9,
         teamLogo: "nick2024logo.jpg",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0003&O=07",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0003&O=07",
         record: {
             record: "8-6",
             winPercentage: ".571",
@@ -53,7 +53,7 @@ export default [
         ownerName: "Kristen Hazen",
         playoffPoints: 559.8,
         teamLogo: "kristen2024logo.jpg",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0004&O=07",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0004&O=07",
         record: {
             record: "8-6",
             winPercentage: ".571",
@@ -65,11 +65,11 @@ export default [
     },
     {
         id: 5,
-        teamName: "Team Brooke-Lynn",
+        teamName: "H-Town Honeys",
         ownerName: "Brooke-Lynn Killingbeck",
         playoffPoints: 396.10,
-        teamLogo: "kylelogo2025.png",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0005&O=07",
+        teamLogo: "brooke_dynasty.png",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0005&O=07",
         record: {
             record: "6-8",
             winPercentage: ".429",
@@ -80,11 +80,11 @@ export default [
     },
     {
         id: 6,
-        teamName: "Team Jake",
+        teamName: "Old Chode",
         ownerName: "Jake Killingbeck",
         playoffPoints: 400.25,
-        teamLogo: "brandon2024logo.jpg",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0006&O=07",
+        teamLogo: "jake_dynasty.png",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0006&O=07",
         record: {
             record: "3-11",
             winPercentage: ".214",
@@ -99,7 +99,7 @@ export default [
         ownerName: "Gary Garcia",
         playoffPoints: 473.80,
         teamLogo: "gary2024logo.jpg",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0007&O=07",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0007&O=07",
         record: {
             record: "5-9",
             winPercentage: ".357",
@@ -114,7 +114,7 @@ export default [
         ownerName: "Chad Marchand",
         playoffPoints: 348.3,
         teamLogo: "chad2025.png",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0008&O=07",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0008&O=07",
         record: {
             record: "11-3",
             winPercentage: ".786",
@@ -130,7 +130,7 @@ export default [
         ownerName: "Jimmy Cunningham",
         playoffPoints: 399.75,
         teamLogo: "jimmy2024logo.jpg",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0009&O=07",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0009&O=07",
         record: {
             record: "4-10",
             winPercentage: ".286",
@@ -145,7 +145,7 @@ export default [
         ownerName: "Jeremy Ogden",
         playoffPoints: 439.35,
         teamLogo: "jeremy2024logo.jpg",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0010&O=07",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0010&O=07",
         record: {
             record: "9-5",
             winPercentage: ".643",
@@ -161,7 +161,7 @@ export default [
         ownerName: "Justin Gutierrez",
         playoffPoints: 247.60,
         teamLogo: "justin_dynasty.jpg",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0011&O=07",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0011&O=07",
         record: {
             record: "7-7",
             winPercentage: ".500",
@@ -176,7 +176,7 @@ export default [
         ownerName: "Met Nagatani",
         playoffPoints: 468.30,
         teamLogo: "met2024logo.jpg",
-        teamUrl: "https://www44.myfantasyleague.com/2025/options?L=63794&F=0012&O=07",
+        teamUrl: "https://www44.myfantasyleague.com/2026/options?L=63794&F=0012&O=07",
         record: {
             record: "7-7",
             winPercentage: ".500",
